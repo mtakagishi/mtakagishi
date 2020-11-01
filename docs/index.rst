edcb9c98c2f4eec54f998c423cf951f82711a1ee
mtakagishi <mtakagishi+github@gimail.com>
2020-11-01 18:13:18 +0900

    初期セットアップ

---
+++ docs/index.rst
@@ -0,0 +1,12 @@
+.. mtakagishi documentation master file, created by
+   sphinx-quickstart on Sun Nov  1 17:40:02 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to mtakagishi's notebook!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+from setuptools import setup, Command
+
+
+class SimpleCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+
+class DocCommand(SimpleCommand):
+    def run(self):
+        subprocess.call(["sphinx-autobuild", "docs", "docs/_build/ja"])
+
+
+class DocEnglishCommand(SimpleCommand):
+    def run(self):
+        subprocess.call(["sphinx-build", "-M", "gettext", "docs", "docs/_build/ja"])
+        subprocess.call(["sphinx-intl", "update", "-d", "docs/locales",
+                         "-p", "_build/ja/gettext", "-l", "en"])
+        subprocess.call(["sphinx-autobuild", "docs",
+                         "docs/_build/en", "-D", "language=en"])
+
+
+setup(
+    use_scm_version=True,
+    cmdclass={
+        "doc": DocCommand,
+        "doc_en": DocEnglishCommand,
+    },
+)
